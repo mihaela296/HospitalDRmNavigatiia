ee6e565f703ff11ba31f5831de2d7ac71b29a443
SASA <SASA@DESKTOP-ATLJLER>
2025-03-16 22:59:35 +0300

    Добавьте файлы проекта.

---
+++ HospitalD/AdminPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HospitalD
+{
+    /// <summary>
+    /// Логика взаимодействия для AdminPage.xaml
+    /// </summary>
+    public partial class AdminPage : Window
+    {
+        private Users _user;
+        public AdminPage(Users user)
+        {
+            InitializeComponent();
+            _user = user;
+        }
+
+        private void Departments_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new DepartmentsPage());
+        }
+
+        private void Staff_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new StaffPage());
+        }
+
+        private void Patients_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new PatientsPage());
+        }
+
+        private void Diagnoses_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new DiagnosesPage());
+        }
+
+        private void MedicalProcedures_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new MedicalProceduresPage());
+        }
+
+        private void Medications_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new MedicationsPage());
+        }
+        private void Logout_Click(object sender, RoutedEventArgs e)
+        {
+            new MainWindow().Show();
+            this.Close();
+        }
+
+    }
+}
---
+++ HospitalD/PatientsPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HospitalD
+{
+    /// <summary>
+    /// Логика взаимодействия для DepartmentsPage.xaml
+    /// </summary>
+    public partial class PatientsPage : Page
+    {
+        public PatientsPage()
+        {
+            InitializeComponent();
+            PatientsDataGrid.ItemsSource = HospitalDRmEntities.GetContext().Patients.ToList();
+        }
+        private void ButtonEdit_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonAdd_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void ButtonDel_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
